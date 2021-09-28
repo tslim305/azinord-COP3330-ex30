f2e21e51585a371661972b2300e5c9ef68b93bdf
tslim305 <timothyazinord@gmail.com>
2021-09-27 21:07:01 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 30 Solution
+ *  Copyright 2021 Timothy Azinord
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        for (int i =1; i<=12; i++){
+            for(int j=1; j<=12; j++){
+                System.out.print((i*j)+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
